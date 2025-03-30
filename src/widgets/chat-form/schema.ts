5eeb12a6bec08a2ba6f2c856041b06e117846442
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-03-30 20:31:12 +0500

    fix

--- src/widgets/chat-form/schema.ts
+++ src/widgets/chat-form/schema.ts
@@ -4,8 +4,8 @@ export const MessageSchema = z.object({
   message: z
     .string()
     .min(1, { message: 'Пожалуйста, введите сообщение.' })
-    .max(250, {
-      message: 'Сообщение не должно превышать 250 символов.',
+    .max(1000, {
+      message: 'Сообщение не должно превышать 1000 символов.',
     }),
 });
 
