ea72e4fb9350b18d526947146521888de81f8d60
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-03-30 20:11:06 +0500

    feat: user list

--- src/routes/chat-page/ui/chat-header.tsx
+++ src/routes/chat-page/ui/chat-header.tsx
@@ -8,7 +8,12 @@ import {
   CardHeader,
   CardTitle,
   Skeleton,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from '@/shared/ui';
+import { User } from 'lucide-react';
 
 export const ChatHeader = ({ chatId }: { chatId: string }) => {
   const { users, isLoading } = useChatMessages(chatId);
@@ -22,13 +27,31 @@ export const ChatHeader = ({ chatId }: { chatId: string }) => {
         <CardTitle className="flex flex-col gap-1">
           <span className="text-2xl font-bold">Комната: {chatId}</span>
         </CardTitle>
-        <div className='h-6'>
+        <div className="h-6">
           {isLoading ? (
             <Skeleton className="w-[150px] h-full bg-message-hover rounded-sm" />
           ) : (
-            <CardDescription>
-              <p className="line-clamp-1 max-w-[200px]">{users.join(', ')}</p>
-            </CardDescription>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <CardDescription>
+                    <p className="line-clamp-1 max-w-[200px]">
+                      {users.join(', ')}
+                    </p>
+                  </CardDescription>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className="font-bold">Участники</p>
+                  <ul>
+                    {users.map((user) => (
+                      <li className="flex items-center gap-1" key={user}>
+                        <User size={12} /> {user}
+                      </li>
+                    ))}
+                  </ul>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           )}
         </div>
       </div>
