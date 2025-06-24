7bfb588a890fdce0e91cae36fa048a97f5a3e97b
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-06-24 18:39:21 +0500

    docs: todo update

--- src/entities/chat-message/ui/metainfo.tsx
+++ src/entities/chat-message/ui/metainfo.tsx
@@ -36,39 +36,41 @@ export const MetaInfo = ({ message }: MetaInfoProps) => {
   }, [message.content]);
 
   return (
-    <div className="mb-1 mt-1.5">
+    <>
       {linkMeta && (
-        <a
-          href={linkMeta.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="border rounded-lg overflow-hidden hover:shadow-sm transition-colors flex items-start py-2 px-3 flex-row justify-between gap-3 bg-card/20 hover:bg-card/40"
-        >
-          {linkMeta.image && !photoError && (
-            <div className="h-full w-full max-w-[70px]">
-              <img
-                onError={() => setPhotoError(true)}
-                src={linkMeta.image}
-                alt={linkMeta.title}
-                className="object-cover h-full w-[70px] rounded-sm"
-              />
-            </div>
-          )}
-          <div className="w-auto">
-            <h4 className="font-medium text-sm line-clamp-2">
-              {linkMeta.title || 'Без названия'}
-            </h4>
-            {linkMeta.description && (
-              <p className="text-[11px] text-muted-foreground mt-1 line-clamp-2">
-                {linkMeta.description}
-              </p>
+        <div className="mb-1 mt-1.5">
+          <a
+            href={linkMeta.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="border rounded-lg overflow-hidden hover:shadow-sm transition-colors flex items-start py-2 px-3 flex-row justify-between gap-3 bg-card/20 hover:bg-card/40"
+          >
+            {linkMeta.image && !photoError && (
+              <div className="h-full w-full max-w-[70px]">
+                <img
+                  onError={() => setPhotoError(true)}
+                  src={linkMeta.image}
+                  alt={linkMeta.title}
+                  className="object-cover h-full w-[70px] rounded-sm"
+                />
+              </div>
             )}
-            <span className="text-xs text-muted-foreground mt-1 block underline">
-              {linkMeta.siteName || new URL(linkMeta.url || '').hostname}
-            </span>
-          </div>
-        </a>
+            <div className="w-auto">
+              <h4 className="font-medium text-sm line-clamp-2">
+                {linkMeta.title || 'Без названия'}
+              </h4>
+              {linkMeta.description && (
+                <p className="text-[11px] text-muted-foreground mt-1 line-clamp-2">
+                  {linkMeta.description}
+                </p>
+              )}
+              <span className="text-xs text-muted-foreground mt-1 block underline">
+                {linkMeta.siteName || new URL(linkMeta.url || '').hostname}
+              </span>
+            </div>
+          </a>
+        </div>
       )}
-    </div>
+    </>
   );
 };
